c3eaf947fed3bea156c020124aafb9c9c4259da5
Lucas Santana <lucas.diassantana@gmail.com>
2026-02-28 15:56:00 -0300

    chore: Remove unrelated files added by hooks

--- apps/web/src/lib/security/index.ts
+++
@@ -1,3 +0,0 @@
-export { securityHeaders } from './headers';
-export { corsHeaders, getAllowedOrigins, isOriginAllowed } from './cors';
-export { sanitizeText, sanitizeHtml, escapeForAttribute } from './sanitize';
